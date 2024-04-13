485b6e6da621c80f1e84263849c7641570861945
akhil-kokkuluru <akhilr37@gmail.com>
2024-04-14 00:09:29 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import Home from './Home Component/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>AKHIL RAJA RAO </h1>
+      <h1 className="text-7xl text-blue-400">AKHIL RAJA RAO </h1>
+      <Home/>
     </div>
   );
 }
